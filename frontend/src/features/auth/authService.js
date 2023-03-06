eca1113a4235c2a51c1bf0a4436c0bff3495e768
Dahl Tamares <dahltamares89@gmail.com>
2023-03-07 00:16:38 +0800

    authService, authSlice and store initialized

---
+++ frontend/src/features/auth/authService.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+// Register user
+const register = async (userData) => {
+    const response = await axios.post("/api/users/register", userData);
+
+    if (response.data) {
+        localStorage.setItem("user", JSON.stringify(response.data));
+    }
+
+    return response.data;
+};
+
+// Login user
+const login = async (userData) => {
+    const response = await axios.post("api/users/login", userData);
+
+    if (response.data) {
+        localStorage.setItem("user", JSON.stringify(response.data));
+    }
+
+    return response.data;
+};
+
+const authService = {
+    register,
+    login,
+};
+
+export default authService;
