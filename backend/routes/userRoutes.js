77b1298711f86e2423623c59479e98092fdd6337
redisgrey <dahltamares89@gmail.com>
2023-03-07 07:20:59 +0800

    forgot-password routes done

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,11 +2,23 @@ const express = require("express");
 
 const router = express.Router();
 
-const { registerUser, loginUser } = require("../controllers/userController");
+const {
+    registerUser,
+    loginUser,
+    forgotPassword,
+    resetPassword,
+    updatePassword,
+} = require("../controllers/userController");
 
 //*ROUTES
 router.post("/register", registerUser);
 
 router.post("/login", loginUser);
+
+router.post("/forgot-password", forgotPassword);
+
+router.get("/reset-password", resetPassword);
+
+router.post("/update-password", updatePassword);
 
 module.exports = router;
