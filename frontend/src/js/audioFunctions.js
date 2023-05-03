c2c038bf02975f7ab220dbb03717982d4d34bfbd
redisgrey <dahltamares89@gmail.com>
2023-05-03 09:20:36 +0800

    small changes

--- frontend/src/js/audioFunctions.js
+++ frontend/src/js/audioFunctions.js
@@ -1058,16 +1058,3 @@ export function bufferToWave(abuffer) {
 
     return new Blob([buffer], { type: "audio/wav" });
 }
-
-function floatTo16BitPCM(output, offset, input) {
-    for (let i = 0; i < input.length; i++, offset += 2) {
-        const s = Math.max(-1, Math.min(1, input[i]));
-        output.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7fff, true);
-    }
-}
-
-function writeString(view, offset, string) {
-    for (let i = 0; i < string.length; i++) {
-        view.setUint8(offset + i, string.charCodeAt(i));
-    }
-}
