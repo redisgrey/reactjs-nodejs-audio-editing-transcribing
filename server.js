fb9f8f8717e8dd7fecedbdcd5273a8031cbfed77
Dahl Tamares <dahltamares89@gmail.com>
2023-05-13 00:35:00 +0800

    server add build folder

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const cors = require("cors");
 
 const dotenv = require("dotenv").config();
 
+const path = require("path");
+
 const PORT = process.env.PORT || 5000;
 
 // DATABASE CONNECTION
@@ -32,5 +34,12 @@ app.use("/api/users", require("./backend/routes/userRoutes"));
 app.use("/api/text-to-speech", require("./backend/routes/textToSpeech"));
 
 app.use("/api/speech-to-text", require("./backend/routes/speechToText"));
+
+app.use(express.static(path.join(`${__dirname}/frontend`, "build")));
+
+// Serve index.html for all routes except the ones defined above
+app.get(/^(?!\/api\/).*/, function (req, res) {
+    res.sendFile(path.join(`${__dirname}/frontend`, "build", "index.html"));
+});
 
 app.listen(PORT, () => console.log(`Server running at PORT ${PORT}`));
